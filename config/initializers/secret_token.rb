e7f56a3b09411a55eff3b163a9fa08baf09fd72e
BennyBooBooBoo <BennyBooBooBoo@gmail.com>
2012-06-03 18:45:45 -0500

    Howdy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Igt::Application.config.secret_token = '60f9e58fda5cadd3060152ffd1a4bfb38278f80bd16080d69598804117c8b691f4124309c5050bc0bb65075120b5115aa211a452d91d1b32449b78d839d9fd5f'
